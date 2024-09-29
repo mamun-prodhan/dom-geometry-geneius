ec541c874d7e56f41fc856935361f0573a583f5e
mamun-prodhan <mamunpr9@gmail.com>
2024-09-30 04:47:11 +0600

    rectangle area implmentd

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,23 @@
 function calculateTriangleArea(){
-    console.log('inside function');
+    // triangle base value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    console.log( base);
+
+    // triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeighText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeighText);
+    console.log( height);
+
+    // calculate triangle area
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    // display triangle area
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+    
+
 }
